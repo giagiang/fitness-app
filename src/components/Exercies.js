97431fa4a4821616c57a57f5961d6e9c7c925381
fastboy <giagiang200@gmail.com>
2023-10-30 23:00:46 +0700

    import exercisedetail to component Exercisedetail

--- src/components/Exercies.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Exercies = () => {
-  return (
-    <div>Exercies</div>
-  )
-}
-
-export default Exercies
--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,32 +1,39 @@
 import React, { useState, useEffect } from "react";
 import { useFetcher, useParams, usePrams } from "react-router-dom";
 import { Box } from "@mui/material";
-import { exerciseOptions, fetchData } from "../utils/fetchData";
+import { exerciseOptions, fetchData, youtubeOptions } from "../utils/fetchData";
 import Details from "../components/Details";
 import ExerciseVideos from "../components/ExerciseVideos";
 import SimilarExercises from "../components/SimilarExercises";
 
 const ExerciseDetail = () => {
   const [exerciseDetail, setExerciseDetail] = useState({});
-  const {id} = useParams();
-  
-  useEffect (()=>{
-    const fetchExerciseData = async () => {   
-    const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com';
-    const youtubeSearchUrl = 'https://youtube-search-and-download.p.rapidapi.com/video/related';
-    const exerciseDetailData = await fetchData (`${exerciseDbUrl}/exercises/${id}`, exerciseOptions);
-    setExerciseDetail(exerciseDetailData);
-    }
-    fetchExerciseData ();
-    },[id]);
+  const [ exerciseVideos, setExerciseVideos] = useState([])
+  const { id } = useParams();
+ 
+  useEffect(() => {
+    const fetchExerciseData = async () => {
+      const exerciseDbUrl = "https://exercisedb.p.rapidapi.com";
+      const youtubeSearchUrl =
+        "https://youtube-search-and-download.p.rapidapi.com/video/related";
+      const exerciseDetailData = await fetchData(
+        `${exerciseDbUrl}/exercises/exercise/${id}`,
+        exerciseOptions
+      );
+      setExerciseDetail(exerciseDetailData);
+
+      const exerciseVideosData = await fetchData (`${youtubeSearchUrl}/search?q=${exerciseDetailData.name}`,youtubeOptions );
+      setExerciseVideos(exerciseVideosData) 
+    };
+    fetchExerciseData();
+  }, [id]);
   return (
     <Box>
-      <Details exerciseDetail = {exerciseDetail} />
-      <ExerciseVideos />
-      <SimilarExercises />
+      <Details exerciseDetail={exerciseDetail} />
+      <ExerciseVideos  exerciseVideos = {exerciseVideos} name = {exerciseDetail.name}/>
+      <SimilarExercises  />
     </Box>
   );
 };
 
 export default ExerciseDetail;
- 
